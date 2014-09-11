1ea2edb682dfc1db82d30c3b478fcc5883f0f3f7
jarusified <jarus3001@gmail.com>
2014-09-11 19:04:24 +0530

    version 0.0.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var imgextension = ['png','jpg'];
+var arr=[];
 module.exports = function(dir){
 	fs.exists(dir,function(exists){
 		if(exists){
@@ -11,11 +12,21 @@ module.exports = function(dir){
 					for(var i=0;i<list.length;i++){
 						// extension check
 						var ext = list[i].split('.').pop();
+						if(ext=='jpg' || ext=='png')     arr.push(list[i]);
 					}
 				}
-			})
-
-			
+				var rand,
+					temp,
+					len=arr.length,
+					ret = arr.slice();
+				while(len){
+					rand=Math.floor(Math.random()*len--);
+					temp=ret[len];
+					ret[len]=ret[rand];
+					ret[rand]=temp;
+				}
+				console.log(ret);
+			});
 		}
-	})
+	});
 }
